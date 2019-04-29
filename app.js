893220f4b534295480141b884ce21ca747ec1edd
RUFINO JOHN AGUILAR <aguilarufino@gmail.com>
2019-04-29 11:11:41 +0800

    production panganud client

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 var app = require('express')();
 var server = require('http').Server(app);
 var request = require('request');
@@ -6,23 +5,124 @@ var io = require('socket.io')(server);
 var iocilent = require('socket.io-client');
 var lguid = 'bukidnon';
 var cloud = iocilent.connect('http://122.54.200.110:9000');
+// var cloud = iocilent.connect('http://localhost:9000');
+var connected = false;
+
 
 server.listen(3000,function(){
 	console.log('SERVER RUNNING AT PORT 3000 WITH LGUID = ' + lguid);
-	//log in to cloud if online
-	cloud.emit('serveronline', lguid);
+	
 
-	// cloud.on('serverrequest'+lguid, function(data,fn){
+	cloud.emit('checkinserveronline', lguid);
+
+	cloud.emit('message', "Hello " + lguid);
+
+	// cloud.on('serverrequestbukidnon', function(data,fn){
+	// 	console.log(data);
+	// 	fn(data);
+	// });
+
+	cloud.on('serverrequest'+lguid, function (params,fn) {
+		console.log("PROCESSING REQUEST FROM :" + params.sender);
+		request.post({
+			url:'http://localhost:8072/osiris3/json/etracs25/' + params.servicename + '.' + params.methodname,
+			json:params
+		},function (error, response, body) {
+			if (!error && response.statusCode == 200) {
+				console.log('SENDING RESPONSE TO : ' +  params.sender)
+				fn(body);
+			}
+		});
+	});
+
+	// cloud.on('serverrequest', function (sender,params) {
+	// 	request.post({
+	// 		url:'http://localhost:8072/osiris3/json/etracs25/' + params.servicename + '.' + params.methodname,
+	// 		json:params
+	// 	},function (error, response, body) {
+	// 		if (!error && response.statusCode == 200) {
+	// 			console.log('sending response to : ' +  sender)
+	// 			cloud.emit('serverresponse',sender,body);
+	// 		}
+	// 	});
+	// });
+
+	// setInterval(function(){
+	// 	request.post({
+	// 		url:'http://localhost:8072/osiris3/json/etracs25/FarmerProfileService.dash',
+	// 		json:[]
+	// 	},function (error, response, body) {
+	// 		if (!error && response.statusCode == 200) {
+	// 				cloud.emit('dash', body);
+	// 			}
+	// 	});
+	// },10000);
+
+	cloud.on('login', (data) => {
+		connected = true;
+		console.log(data.numServers + " servers online");
+	});
+
+	cloud.on('serveronline', (data) => {
+		console.log(data.servername + ' is connected');
+	});
+
+	cloud.on('serveroffline', (data) => {
+		console.log(data.servername + ' is disconnected');
+	});
+
+	cloud.on('disconnect', () => {
+		console.log('you have been disconnected');
+	});
+
+	cloud.on('message', (data) => {
+		console.log("message from " + data.servername +" : "+ data.message);
+	});
+
+	cloud.on('reconnect', () => {
+		console.log('you have been reconnected');
+		if (lguid) {
+			cloud.emit('checkinserveronline', lguid);
+		}
+	});
+
+	cloud.on('reconnect_error', () => {
+		console.log('attempt to reconnect has failed');
+	});
+
+});
+
+// cloud.emit('serverrequest', 'bukidnon', {
+// 	servicename	: 'FarmerProfileService',
+// 	methodname	: 'getFarmersList',
+// 	params		: {
+// 		_start : 0,
+// 		_limit : 1
+// 	}
+// },function(data){
+// 	console.log(data);
+// });
+
+// cloud.on('remotecall', (data,fn) => {
+// 	console.log(data);
+// 	fn("TEST");
+// });
+
+//log in to cloud if online
+	// cloud.emit('checkinserveronline', lguid);
+
+	// cloud.on('requestdata'+lguid, function(data,fn){
 	// 	request.post({
 	// 		url:'http://localhost:8072/osiris3/json/etracs25/TagabukidHRMISDashReportService.getCountByQuery',
 	// 		json:data
 	// 	},function (error, response, body) {
 	// 	    if (!error && response.statusCode == 200) {
-	// 	       console.log(body); 
+	// 	       console.log(body);
 	// 	       fn(body);
 	// 	     }
 	// 	});
 	// });
+	
 	
 	// cloud.on('servertestdata'+lguid, function(data,fn){
 	// 	console.log("FETCH DATA FROM "+ lguid +" USING SAMPLE PARAMETER = " + data.sampleparam);
@@ -31,24 +131,18 @@ server.listen(3000,function(){
 	// 	fn(sampledata);
 	// });
 
-	setInterval(function(){
-		request.post({
-			url:'http://localhost:8072/osiris3/json/etracs25/TagabukidHRMISDashReportService.getCountByQuery',
-			json:[]
-		},function (error, response, body) {
-			if (!error && response.statusCode == 200) {
-					// console.log(body); 
-					// console.log(body[0].code);
-					cloud.emit('Message', body);
-				}
-		});
-	},10000);
-
-	cloud.on('Message', (data) => {
-		// console.log('receive : ' + data);
-	});
-
-});
+	// setInterval(function(){
+	// 	request.post({
+	// 		url:'http://localhost:8072/osiris3/json/etracs25/TagabukidHRMISDashReportService.getCountByQuery',
+	// 		json:[]
+	// 	},function (error, response, body) {
+	// 		if (!error && response.statusCode == 200) {
+	// 				// console.log(body); 
+	// 				// console.log(body[0].code);
+	// 				cloud.emit('Message', body);
+	// 			}
+	// 	});
+	// },10000);
 
 	// cloud.on('serverrequest'+lguid, function(data,fn){
 	// 	request.post({

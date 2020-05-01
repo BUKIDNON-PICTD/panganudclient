67e0a3721c92710c9776b3f1f80120bab7cfbc83
RUFINO JOHN AGUILAR <aguilarufino@gmail.com>
2020-05-01 12:21:32 +0800

    adjust payload size

--- src/server.js
+++ src/server.js
@@ -4,7 +4,8 @@ const passport   = require('passport');
 const cors       = require('cors');
 const app        = express();
 app.use(cors());
-
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
 
 
 
